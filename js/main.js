efaeb0166f3726dbf52eb8d2aff4e3d47cdf9b9a
Francesca <fracassani@gmail.com>
2020-10-26 15:27:59 +0100

    Esercizio svolto

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+/**MOSTRARE / NASCONDERE ELEMENTI */
+
 var lista = $(".header-right");
 
 lista.click( function () {
     $("ul").show();
+    lista.click( function () {
+        $("ul").hide(); 
+    })
 })
+
+
+
+
+
 
 
